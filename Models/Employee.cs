210b133db863c34de63c88b06cc23eb205d82d1c
naseem <naseemhijazi.786786@gmail.com>
2022-10-14 01:51:29 +0500

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Application_Asp.net_core_MVC.Models
+{
+    public class Employee
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_Application_Asp.net_core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
